5ea76f28924e31babbf862c25c21d8e0ac605f88
Jared Eaton <jxe008@uark.edu>
2012-02-14 13:18:35 -0600

    i fixed several simple problems and implemented a few things
    1. fixed how he walked off axis
    2. fixed the forward/backward motion
    3. implemented physics and colission detection

--- SoftwareEngProj/Assets/walk.js
+++ SoftwareEngProj/Assets/walk.js
@@ -6,11 +6,11 @@ function Update () {
 	
 	if(right){
 		animation.Play("Walk");
-		transform.Translate(0,0,-Input.GetAxis("Horizontal") * .1);
+		transform.Translate(0,0,.1);
 	}
 	
 	else if(left){
 		animation.Play("Walk");
-		transform.Translate(0,0,Input.GetAxis("Vertical") * .1);
+		transform.Translate(0,0,-0.1);
 	}
 }
