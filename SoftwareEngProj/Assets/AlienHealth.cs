6a0f436d0056c522e430d39751bad7420b669219
Cody Klomfas <Cklomfas@uark.edu>
2012-04-13 16:54:10 -0500

    Added some Alien AI and code, along with a lot of other files that it says i need to comit.

---
+++ SoftwareEngProj/Assets/AlienHealth.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class AlienHealth : MonoBehaviour {
+	public float Health;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
